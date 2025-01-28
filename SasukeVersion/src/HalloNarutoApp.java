434cefa15906850626a219b7b11a12fbadc15f33
tuttics <52403815@edu.campus02.at>
2025-01-28 21:10:54 +0100

    Zeile 8 hinzugefügt - Naruto VS Sasuke

---
+++ SasukeVersion/src/HalloNarutoApp.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class HalloNarutoApp{
+    public static void main(String[] args) {
+
+        System.out.println("Das ist mein erstes Narutospiel");
+        System.out.println();
+        System.out.println("Naruto VS Sasuke");
+
+    }
+
+
+
+}
